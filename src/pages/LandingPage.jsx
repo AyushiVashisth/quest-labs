9f8a7f33af176637502cf527816e72ad2461163f
Ayushi Vashisth <ayushivashisth22@gmail.com>
2024-06-19 02:03:36 +0530

    add react-router-dom

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LandingPage = () => {
+  return (
+    <div className="bg-black text-white min-h-screen w-full relative m-auto overflow-hidden">
+      LandingPage
+    </div>
+  );
+};
+
+export default LandingPage;
